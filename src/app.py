35c5ab52b48fe90b4136d05eeea87116a1c0e83a
Prashanth Ravoor <prash.ravoor125@gmail.com>
2018-12-13 18:57:48 +0530

    GUI Done for Create List, Select List, Insert Node, Show List, List Names

--- src/app.py
+++ src/app.py
@@ -8,6 +8,9 @@ import time
 from random import randint
 import tkinter.simpledialog
 
+logger = logging.getLogger()
+
+
 class Node(object):
     def __init__(self, value=None):
         self.val = value
@@ -69,16 +72,6 @@ class SinglyLinkedList(object):
             prev = node
 
 
-parser = ArgumentParser()
-logLevel = "DEBUG"
-parser.add_argument('--log')
-args = parser.parse_args()
-logLevel = args.log
-
-if logLevel != None:
-    numLogLevel = getattr(logging, logLevel.upper())
-    logging.basicConfig(level=numLogLevel)
-
 #ll = SinglyLinkedSkipList("Test")
 
 #t1 = time.time()
@@ -96,7 +89,7 @@ if logLevel != None:
 class Application(pygubu.TkApplication):
     def __init__(self, master):
         self.builder = builder = pygubu.Builder()
-        #master.withdraw()
+        # master.withdraw()
         builder.add_from_file('../GUI.ui')
         self.toplevel = builder.get_object('toplevel', master)
         # Connect Delete event to a toplevel window
@@ -105,7 +98,17 @@ class Application(pygubu.TkApplication):
         builder.connect_callbacks(self)
         self.lists = {}
         self.trace_mode = False
+        self.selected_list = None
+
         self.status_text = builder.get_object("statustext")
+        self.num_of_nodes = builder.get_object('numnodes')
+        self.num_of_levels = builder.get_object('numlevels')
+        self.find_nodes_in_level_in = builder.get_object('findnodesinlevel')
+        self.find_nodes_in_level_out = builder.get_object('numnodesinlevel')
+        self.selected_skip_list = builder.get_object('selectedskiplist')
+        self.set_trace = builder.get_object('tracemode')
+
+        self.clearAll()
 
     def quit(self, event=None):
         self.toplevel.quit()
@@ -119,10 +122,23 @@ class Application(pygubu.TkApplication):
         self.status_text.see(tk.END)
         self.status_text.configure(state="disabled")
 
-    def clearAll(self):
+    def clearStatus(self):
         self.status_text.configure(state="normal")
         self.status_text.delete("1.0", tk.END)
         self.status_text.configure(state="disabled")
+
+    def clearLabels(self):
+        self.num_of_nodes['text'] = ''
+        self.num_of_levels['text'] = ''
+        self.find_nodes_in_level_in.delete('0', tk.END)
+        self.find_nodes_in_level_in.insert(tk.END, 0)
+        self.find_nodes_in_level_out['text'] = 0
+        self.selected_skip_list['text'] = ''
+        self.selected_list = None
+
+    def clearAll(self):
+        self.clearStatus()
+        self.clearLabels()
 
     def on_close_window(self, event=None):
         print('On close window')
@@ -130,55 +146,152 @@ class Application(pygubu.TkApplication):
         self.toplevel.master.destroy()
 
     def CreateList(self):
-        result = tk.simpledialog.askstring('Skip List Name', 'Enter Skip List Name', parent=None)
+        result = tk.simpledialog.askstring(
+            'Skip List Name', 'Enter Skip List Name', parent=None)
         if not result:
             self.appendMessage("No List was Created")
             return None
         try:
             self.CreateListNamed(result)
-            self.appendMessage("Successfully created skip list with name {}".format(result))
+            self.appendMessage(
+                "Successfully created skip list with name {}".format(result))
         except ValueError:
-            self.appendMessage("Failed to create skip list {}, it already exists!".format(result))
+            self.appendMessage(
+                "Failed to create skip list {}, it already exists!".format(result))
 
     def CreateListNamed(self, name, mode="single"):
         try:
             self.GetListNamed(name)
-            logging.error(
+            logger.error(
                 "There is already a skip list with name: {0}".format(name))
         except ValueError:
-            logging.info("Created new Skip List")
+            logger.info("Created new Skip List")
             self.lists[name] = SinglyLinkedSkipList(name)
             return
         raise ValueError
 
+    def ListNames(self):
+        self.appendMessage(
+            "Currently Available Lists: {}".format(list(self.lists.keys())))
+
+    def DeleteList(self):
+        result = tk.simpledialog.askstring(
+            'Delete Skip List', 'Enter Skip List Name', parent=None)
+        if not result:
+            self.appendMessage("No List was Deleted")
+            return None
+        try:
+            self.lists.pop(result)
+            self.appendMessage(
+                "Successfully deleted skip list with name {}".format(result))
+            if self.selected_list.name == result:
+                self.clearLabels()
+        except KeyError:
+            self.appendMessage("Skip List {} does not exist".format(result))
+
+    def getNodesInLevelVal(self):
+        level = self.builder.get_variable('nodes_in_level').get()
+        try:
+            level = int(level)
+            if level < 0:
+                raise ValueError
+            return level
+        except ValueError:
+            self.appendMessage("Invalid Integer {}".format(level))
+            raise ValueError
+
+    def setLabels(self):
+        logger.debug("Set Labels called")
+        if self.selected_list:
+            self.num_of_nodes['text'] = "{}".format(
+                self.selected_list.length())
+            self.num_of_levels['text'] = "{}".format(self.selected_list.levels)
+            try:
+                self.find_nodes_in_level_out['text'] = "{}".format(
+                    self.selected_list.getNodesInLevel(self.getNodesInLevelVal()))
+            except ValueError:
+                self.appendMessage("The value entered is not valid")
+
+    def SelectSkipList(self):
+        result = tk.simpledialog.askstring(
+            'Select Skip List', 'Enter Skip List Name', parent=None)
+        if not result:
+            self.appendMessage("No List was Selected")
+            return None
+        try:
+            self.selected_list = self.lists[result]
+            self.appendMessage("Skip List {} selected".format(result))
+            self.selected_skip_list['text'] = result
+            self.setLabels()
+        except KeyError:
+            self.appendMessage("Skip List {} does not exist".format(result))
+
+    def FindNumNodesInLevel(self):
+        if not self.selected_list:
+            self.appendMessage("You need to select a skip list first")
+            return
+
+        self.setLabels()
+
+    def ShowNodesInLevel(self):
+        if not self.selected_list:
+            self.appendMessage("You need to select a skip list first")
+            return
+
+        try:
+            level = self.getNodesInLevelVal()
+        except ValueError:
+            self.appendMessage("The value entered is invalid")
+            return
+
+        self.appendMessage("Displaying Nodes in Level {}\n\n{}".format(
+            level, self.selected_list.getLevelTraversal(level)))
+
+    def InsertItem(self):
+        if not self.selected_list:
+            self.appendMessage("You need to select a skip list first")
+            return
+
+        result = tk.simpledialog.askinteger(
+            'Insert into Skip List', 'Enter Integer Value', parent=None)
+        self.appendMessage("Added item {} into Skip List {}".format(
+            result, self.selected_list.name))
+
+        if not result:
+            self.appendMessage("No item was added")
+            return
+
+        self.selected_list.insert(result)
+        self.setLabels()
+        if self.trace_mode:
+            self.appendMessage("{}".format(self.selected_list))
+
     def GetList(self):
-        name = tk.simpledialog.askstring("Get List", "List Name")
+        if not self.selected_list:
+            name = tk.simpledialog.askstring("Get List", "List Name")
+        else:
+            name = self.selected_list.name
         try:
             self.appendMessage(self.GetListNamed(name))
         except ValueError:
             self.appendMessage("List {} does not exist".format(name))
 
-
     def GetListNamed(self, name):
-        logging.info("Getting list {0}".format(name))
+        logger.info("Getting list {0}".format(name))
         if not name in self.lists:
             raise ValueError
 
         return self.lists[name]
 
     def ShowListNamed(self, name):
-        logging.info("Showing list {0}".format(name))
+        logger.info("Showing list {0}".format(name))
         return "{0}".format(self.GetListNamed(name))
-
-    def DeleteList(self, name):
-        logging.info("Deleting list {0}".format(name))
-        self.lists.popitem(name)
 
     def CreateData(self, filename, numKeys):
         f = open(filename, "w", newline="")
         writer = csv.writer(f)
 
-        logging.info("Creating {0} keys in file {1}".format(numKeys, filename))
+        logger.info("Creating {0} keys in file {1}".format(numKeys, filename))
         keys = []
         for i in range(1, numKeys):
             keys.append(randint(1000000000, 9999999999))
@@ -190,18 +303,30 @@ class Application(pygubu.TkApplication):
         reader = csv.reader(f)
         keys = []
 
-        logging.info("Reading all integers from file {0}".format(filename))
+        logger.info("Reading all integers from file {0}".format(filename))
         for key in reader:
             keys.append(key)
 
-        logging.info("Found {0} keys in file {1}".format(len(keys), filename))
+        logger.info("Found {0} keys in file {1}".format(len(keys), filename))
         return keys
 
-    def SetTrace(self, value):
-        self.trace_mode = value
+    def SetTrace(self):
+        self.trace_mode = self.builder.get_variable('set_trace').get()
+        print("Set Trace Mode to {}".format(self.trace_mode))
 
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+    logLevel = "DEBUG"
+    parser.add_argument('--log')
+    args = parser.parse_args()
+    logLevel = args.log
+
+    if logLevel != None:
+        numLogLevel = getattr(logging, logLevel.upper())
+        logging.basicConfig(level=numLogLevel)
+        logger.setLevel(numLogLevel)
+
     root = tk.Tk()
     app = Application(root)
     app.run()
--- src/skip_list.py
+++ src/skip_list.py
@@ -2,10 +2,11 @@ import logging
 import random
 from math import log2, floor
 
+logger = logging.getLogger()
 
 class SkipListNode(object):
     def __init__(self, value):
-        logging.debug(
+        logger.debug(
             "Created A new Skip List node with value {0}".format(value))
         self.value = value
         self.prev = None
@@ -44,14 +45,14 @@ class SkipListNode(object):
         while current.down != None:
             current = current.down
 
-        logging.debug(
+        logger.debug(
             "The Zero Level node was found to be: {0}".format(current))
         return current
 
 
 class SinglyLinkedSkipList(object):
     def __init__(self, name):
-        logging.debug(
+        logger.debug(
             "Creating a new Singly Linked Skip List of name {0}".format(name))
         self.name = name
         self.heads = []
@@ -59,7 +60,7 @@ class SinglyLinkedSkipList(object):
         self.size = 0
 
     def getSimpleTraversal(self, node):
-        logging.debug("Starting simple traversal from node {0}".format(node))
+        logger.debug("Starting simple traversal from node {0}".format(node))
         str = ""
         while node != None:
             str += "{0} ".format(node)
@@ -73,7 +74,7 @@ class SinglyLinkedSkipList(object):
 
         level = 0
         for current in self.heads:
-            logging.debug("Printing level {0}".format(level))
+            logger.debug("Printing level {0}".format(level))
             str += "Level {0}: [ ".format(level)
             str += self.getSimpleTraversal(current)
             str += "]\n"
@@ -97,19 +98,41 @@ class SinglyLinkedSkipList(object):
             return tmp
 
     def length(self):
-        logging.debug("Current length of skip list {0} is {1}".format(
+        logger.debug("Current length of skip list {0} is {1}".format(
             self.name, self.size))
         return self.size
 
+    def getNodesInLevel(self, level):
+        if level < 0 or level > self.levels:
+            return -1
+
+        i = 0
+        current = self.heads[level]
+        while current:
+            i += 1
+            current = current.next
+        logger.debug("Found {} nodes in level {}".format(i, level))
+        return i
+
+    def getLevelTraversal(self, level):
+        if level < -1 or level > self.levels:
+            return "Invalid level {}, current number of levels is {}".format(level, self.levels)
+
+        if level == -1:
+            return ""
+
+        return self.getSimpleTraversal(self.heads[level])
+
+
     def find_top(self):
         if self.size == 0:
-            logging.debug("Find Top returns {0}".format(None))
+            logger.debug("Find Top returns {0}".format(None))
             return None
-        logging.debug("Find Top returns {0}".format(self.heads[self.levels]))
+        logger.debug("Find Top returns {0}".format(self.heads[self.levels]))
         return self.heads[self.levels]
 
     def find_pred(self, value, stop_at_level=0):
-        logging.debug(
+        logger.debug(
             "Find_Pred for value {0} in Skip List {1} at level {2}".format(value, self.name, stop_at_level))
         node = SkipListNode(value)
 
@@ -120,31 +143,31 @@ class SinglyLinkedSkipList(object):
 
         while level >= stop_at_level:
             prev = None
-            logging.debug(
+            logger.debug(
                 "Traversing level {0} in skip list {1}".format(level, self.name))
             while current and current < node:
                 prev = current
                 current = current.next
 
-            logging.debug("Stopped at {0} on level {1}, prev is {2}".format(
+            logger.debug("Stopped at {0} on level {1}, prev is {2}".format(
                 current, level, prev))
             if current and current < node:
-                logging.debug("Moving down from node {0} at level {1} to node {2}".format(current, level, current.down))
+                logger.debug("Moving down from node {0} at level {1} to node {2}".format(current, level, current.down))
                 current = current.down
             elif prev:
-                logging.debug("Prev: Moving down from node {0} at level {1} to node {2}".format(prev, level, prev.down))
+                logger.debug("Prev: Moving down from node {0} at level {1} to node {2}".format(prev, level, prev.down))
                 current = prev.down
             else:
                 # Reset to head of next level
                 current = self.heads[level - 1]
             level -= 1
 
-        logging.debug(
+        logger.debug(
             "Returning {0} as predecessor for value {1}".format(prev, value))
         return prev
 
     def simple_insert(self, startNode, value):
-        logging.debug("Simple Insert of value {0} in Skip List {1} starting from node {2}".format(
+        logger.debug("Simple Insert of value {0} in Skip List {1} starting from node {2}".format(
             value, self.name, startNode))
         node = SkipListNode(value)
 
@@ -155,12 +178,12 @@ class SinglyLinkedSkipList(object):
             current = current.next
 
         if prev == None:
-            logging.debug(
+            logger.debug(
                 "Value {0} is the least value in the list".format(value))
             node.next = startNode
             startNode = node
         else:
-            logging.debug(
+            logger.debug(
                 "Found Previous Node to be {0} for value {1}".format(prev, value))
             node.next = prev.next
             prev.next = node
@@ -168,7 +191,7 @@ class SinglyLinkedSkipList(object):
         return startNode
 
     def insert(self, value):
-        logging.debug(
+        logger.debug(
             "Starting Insert on list {0} for value {1}".format(self.name, value))
 
         # Start from top most level, find where to insert
@@ -187,7 +210,7 @@ class SinglyLinkedSkipList(object):
         else:
             num_of_levels = random.randint(0, floor(log2(self.size)))
 
-        logging.info("Node {0} will be in {1} levels".format(
+        logger.info("Node {0} will be in {1} levels".format(
             value, num_of_levels))
 
         level = 0
@@ -196,21 +219,21 @@ class SinglyLinkedSkipList(object):
             prev = self.find_pred(value, level)
             node = SkipListNode(value)
             if not prev:
-                logging.debug(
+                logger.debug(
                     "Node {0} is the least value in the list at level {1}".format(value, level))
                 # Insert at head of level
                 if len(self.heads) <= level:
-                    logging.debug("Adding a head node at level {0}")
+                    logger.debug("Adding a head node at level {0}")
                     self.heads.append(None)
                     self.levels += 1
 
                 node.next = self.heads[level]
                 self.heads[level] = node
 
-                logging.debug("Current Heads: {0}, Levels: {1}".format(self.heads, self.levels))
+                logger.debug("Current Heads: {0}, Levels: {1}".format(self.heads, self.levels))
 
             else:
-                logging.debug(
+                logger.debug(
                     "Node {0} will be inserted after {1} at level {2}".format(value, prev, level))
                 node.next = prev.next
                 prev.next = node
